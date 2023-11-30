758f020345d222b8be176d368f1988bd072a7027
Joan Gabriel Salas Silva <82409450+DarkRose73@users.noreply.github.com>
2023-11-30 19:28:32 -0300

    show provider messages

--- lib/ui/screens/chat/chat_screen.dart
+++ lib/ui/screens/chat/chat_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:yes_no_maybe_app/domain/entities/message.dart';
+import 'package:yes_no_maybe_app/ui/providers/chat_provider.dart';
 import 'package:yes_no_maybe_app/ui/widgets/chat/her_message_bubble.dart';
 import 'package:yes_no_maybe_app/ui/widgets/chat/my_message_bubble.dart';
 import 'package:yes_no_maybe_app/ui/widgets/shared/message_field_box.dart';
@@ -29,6 +32,7 @@ class ChatScreen extends StatelessWidget {
 class _ChatView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final chatProvider = context.watch<ChatProvider>();
     return SafeArea(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10),
@@ -37,12 +41,15 @@ class _ChatView extends StatelessWidget {
             Expanded(
               child: ListView.builder(
                 itemBuilder: (context, index) {
-                  return index.isOdd ? MyMessageBubble() : HerMessageBubble();
+                  final message = chatProvider.messageList[index];
+                  return message.fromWho == FromWho.hers
+                      ? const HerMessageBubble()
+                      : const MyMessageBubble();
                 },
-                itemCount: 100,
+                itemCount: chatProvider.messageList.length,
               ),
             ),
-            MessageFieldBox()
+            const MessageFieldBox()
           ],
         ),
       ),
